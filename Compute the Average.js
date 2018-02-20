c814fd0aae6c3285103cf6fe07d3075149099a04
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-02-20 15:00:51 -0500

    Computer the Average Function Completed

---
+++ Compute the Average.js
@@ -0,0 +1,8 @@
+//var numbers = [23, 52, 11, 2, 90, 40, 18, 38];
+var numbers = [1, 5];
+var total = 0;
+numbers.forEach(function(num){
+    total += num
+});
+
+console.log(total / numbers.length);
